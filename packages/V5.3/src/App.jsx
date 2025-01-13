5acf4cee812152f61d505af3cf40ce72e59ac295
Shao Clean <sczclean@gmail.com>
2025-01-13 15:47:14 +0800

    chore: 换原注释内容

--- packages/V5.3/src/App.jsx
+++ packages/V5.3/src/App.jsx
@@ -1,6 +1,6 @@
 import { useState, useRef } from 'react';
 import './App.css';
-import { useMount, useRequest, useUpdate, useUpdateEffect } from 'ahooks';
+import { useMount, useRequest, useUpdateEffect } from 'ahooks';
 import * as echarts from 'echarts';
 
 document.title = echarts.version;
@@ -30,23 +30,23 @@ function App() {
                         color: '#99A6B6',
                     },
                 },
-                // formatter(_, index) {
-                //     let yLabel = '';
-                //     if (index === 0) {
-                //         yLabel = '0';
-                //     } else if (index === 1) {
-                //         yLabel = '1';
-                //     } else if (index === 2) {
-                //         yLabel = '2';
-                //     } else if (index === 3) {
-                //         yLabel = '3';
-                //     } else if (index === 4) {
-                //         yLabel = '4';
-                //     } else if (index === 5) {
-                //         yLabel = '5';
-                //     }
-                //     return yLabel;
-                // },
+                formatter(_, index) {
+                    let yLabel = '';
+                    if (index === 0) {
+                        yLabel = '0';
+                    } else if (index === 1) {
+                        yLabel = '1';
+                    } else if (index === 2) {
+                        yLabel = '2';
+                    } else if (index === 3) {
+                        yLabel = '3';
+                    } else if (index === 4) {
+                        yLabel = '4';
+                    } else if (index === 5) {
+                        yLabel = '5';
+                    }
+                    return yLabel;
+                },
             },
             series: [
                 {
